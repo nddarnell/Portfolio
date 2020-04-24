ff8e6bb4495b19c74bf8f61e901dc557a7ca1b34
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-04-23 22:05:45 -0700

    Added linkedIn and github buttons, updated mediaqueries for mobile alignment added tranparent background behind about me text for ease of visibility

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,18 @@
 function scratchProject(){
-    location.href="https://scratch.mit.edu/projects/365628026"
+    window.open("https://scratch.mit.edu/projects/365628026")
 }
 function hungryProject(){
-    location.href="https://nddarnell.github.io/Hungry_or_Bored-Project-1/"
+    window.open("https://nddarnell.github.io/Hungry_or_Bored-Project-1/")
 }
 function weatherProject(){
-    location.href="https://nddarnell.github.io/Week6-Homework-5_Day_Weather_Forecast/"
+    window.open("https://nddarnell.github.io/Week6-Homework-5_Day_Weather_Forecast/")
 }
 function schedulerProject(){
-    location.href="https://nddarnell.github.io/Week5-Homework-Daily_Planner/"
+    window.open("https://nddarnell.github.io/Week5-Homework-Daily_Planner/")
+}
+function linkedIn(){
+    window.open("https://www.linkedin.com/in/nathan-darnell-408169a8/")
+}
+function github(){
+    window.open("https://github.com/nddarnell")
 }
