8faf8e58395f42d1206164b8863d984ea2e3a0ca
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-24 20:05:21 -0700

    404 page added

---
+++ portfolio/src/components/pages/PageNotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function PageError(){
+    return(
+        <>
+            <div className="mt-5">
+                <h2>404 Error</h2>
+                <hr/>
+                <h3>The Page You Are Looking For Can Not Be Found</h3>
+            </div>
+        </>
+    )
+}
+
+export default PageError;
