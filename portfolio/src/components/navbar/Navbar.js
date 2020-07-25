dfdc353ea1438063eaac03bf6e52595dabe189d4
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-24 19:26:58 -0700

    Navbar created inside navbar.js

--- portfolio/src/components/navbar/Navbar.js
+++ portfolio/src/components/navbar/Navbar.js
@@ -1,27 +1,28 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 function Navbar(){
     return (
         <>
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-                <a class="navbar-brand" href="./index.html">Nathaniel Darnell</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+                <Link to="/" className="navbar-brand">Nathaniel Darnell</Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./portfolio.html">Portfolio</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <Link to="/projects" className="nav-link">Portfolio</Link>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./contact.html">Contact</a>
+                        <li className="nav-item active">
+                            <Link to="/contact" className="nav-link">Contact</Link>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./Assets/Nathaniel_Darnell_Resume.pdf" target="_blank">Resume</a>
+                        <li className="nav-item active">
+                            <Link to="../../../Nathaniel_Darnell_Resume.pdf" className="nav-link">Resume</Link>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./index.html">About <span class="sr-only">(current)</span></a>
+                        <li className="nav-item active">
+                            <Link to="/" className="nav-link">About</Link>
                         </li>
                     </ul>
                 </div>
