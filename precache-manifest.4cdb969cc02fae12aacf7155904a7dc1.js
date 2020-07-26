feffda983b6a2154fb6d340ccc8af5ce3e02947c
Nathan Darnell <darnell.nathaniel95@gmail.com>
2020-07-25 17:45:47 -0700

    Updates

---
+++ precache-manifest.4cdb969cc02fae12aacf7155904a7dc1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3aa81eef2e965ea4a7025cc748635b",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "49593a5b7353bb671974",
+    "url": "/Portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "602f17f022e7436e7387",
+    "url": "/Portfolio/static/css/main.fb9ec468.chunk.css"
+  },
+  {
+    "revision": "49593a5b7353bb671974",
+    "url": "/Portfolio/static/js/2.3254ff46.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.3254ff46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602f17f022e7436e7387",
+    "url": "/Portfolio/static/js/main.59f0c10f.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "0801e00465f1255db720472f38476e05",
+    "url": "/Portfolio/static/media/Hungry_or_Bored.0801e004.png"
+  },
+  {
+    "revision": "54f5bf41774f350e7f3f4f5ed60f4707",
+    "url": "/Portfolio/static/media/Scheduler.54f5bf41.png"
+  },
+  {
+    "revision": "9932b1df08d92adcef9ddd0a95342888",
+    "url": "/Portfolio/static/media/Weather-Forecast.9932b1df.png"
+  },
+  {
+    "revision": "99cf34d664259725c3520234cf5bc1a6",
+    "url": "/Portfolio/static/media/burger-project.99cf34d6.png"
+  },
+  {
+    "revision": "7edee4dc9410fbf25f20d9fb6a46e095",
+    "url": "/Portfolio/static/media/directory.7edee4dc.png"
+  },
+  {
+    "revision": "26321cd8c62385151ab84c4769abb5f9",
+    "url": "/Portfolio/static/media/movie-rater-project.26321cd8.png"
+  },
+  {
+    "revision": "288de2e668c12f3074b612af9753d4e5",
+    "url": "/Portfolio/static/media/profilepic.288de2e6.png"
+  },
+  {
+    "revision": "075aa33f8ac4e2de25531ade1dbb582b",
+    "url": "/Portfolio/static/media/yuma-background-2.075aa33f.jpg"
+  }
+]);
